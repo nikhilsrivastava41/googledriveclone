c36e15b7b76d136351cf1d6509343028b2b23f23
Nikhil Srivastava <nikhilsrivastava@Nikhils-MacBook-Air.local>
2021-02-27 18:12:07 +0530

    header done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
-
+import Header from './components/header';
 function App() {
+  // authentication
   return (
     <div className="App">
-      
+      {/* header */}
+      <Header/>
+      {/* if auth: true
+        sidebar
+        files view
+        sideIcons
+      */}
+
+      {/* if auth: false
+        log in
+      */}
     </div>
   );
 }
---
+++ src/components/header/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import GDlogo from '../../media/drive_icon.png';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import SearchIcon from '@material-ui/icons/Search';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import SettingsIcon from '@material-ui/icons/Settings';
+import AppsIcon from '@material-ui/icons/Apps';
+import '../../styles/Header.css';
+const index = () => {
+    return (
+        <div className="header">
+            <div className="header__logo">
+                <img src={GDlogo} alt=""/>
+                <span>Drive</span>
+            </div>
+            <div className="header__searchContainer">
+                <div className="header__searchBar">
+                    <SearchIcon/>
+                    <input type="text" placeholder="Search in Drive"/>
+                    <ExpandMoreIcon/>
+                </div>
+            </div>
+            <div className="header__icons">
+                <span>
+                    <HelpOutlineIcon/>
+                    <SettingsIcon/>
+                </span>
+                <AppsIcon/>
+                <img src="" alt="user image"/>
+            </div>
+        </div>
+    )
+}
+
+export default index
